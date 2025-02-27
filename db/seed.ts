9bffa9c60616bbb716ba2fc1f737f84c086564ce
Bryan Dizon <dizon_danielle@yahoo.com>
2025-02-27 11:45:00 +0800

    added prisma user seed data

--- db/seed.ts
+++ db/seed.ts
@@ -6,9 +6,18 @@ const main = async () => {
 
   //delete all data in the produts table
   await prisma.product.deleteMany();
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationToken.deleteMany();
+  await prisma.user.deleteMany();
 
   //create product
   await prisma.product.createMany({ data: sampleData.products });
+  await prisma.user.createMany({ data: sampleData.users });
+
+  console.log("Database seeded successfully")
+
+  //npx tsx ./db/seed
 };
 
 main();
