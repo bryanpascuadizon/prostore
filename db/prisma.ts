06a793c08d400659e9fccb0f4af9822e84c484ab
Bryan Dizon <dizon_danielle@yahoo.com>
2025-04-10 21:00:57 +0800

    implement format utility functions

--- db/prisma.ts
+++ db/prisma.ts
@@ -14,81 +14,78 @@ const pool = new Pool({ connectionString });
 // Instantiates the Prisma adapter using the Neon connection pool to handle the connection between Prisma and Neon.
 const adapter = new PrismaNeon(pool);
 
-// Extends the PrismaClient with a custom result transformer to convert the price and rating fields to strings.
-// export const prisma = new PrismaClient({ adapter }).$extends({
-//   result: {
-//     product: {
-//       price: {
-//         compute(product: Product) {
-//           return product.price.toString();
-//         },
-//       },
-//       rating: {
-//         compute(product: Product) {
-//           return product.rating.toString();
-//         },
-//       },
-//     },
-//     cart: {
-//       itemsPrice: {
-//         needs: { itemsPrice: true },
-//         compute(cart) {
-//           return cart.itemsPrice.toString();
-//         },
-//       },
-//       shippingPrice: {
-//         needs: { shippingPrice: true },
-//         compute(cart) {
-//           return cart.shippingPrice.toString();
-//         },
-//       },
-//       taxPrice: {
-//         needs: { taxPrice: true },
-//         compute(cart) {
-//           return cart.taxPrice.toString();
-//         },
-//       },
-//       totalPrice: {
-//         needs: { totalPrice: true },
-//         compute(cart) {
-//           return cart.totalPrice.toString();
-//         },
-//       },
-//     },
-//     order: {
-//       itemsPrice: {
-//         needs: { itemsPrice: true },
-//         compute(cart) {
-//           return cart.itemsPrice.toString();
-//         },
-//       },
-//       shippingPrice: {
-//         needs: { shippingPrice: true },
-//         compute(cart) {
-//           return cart.shippingPrice.toString();
-//         },
-//       },
-//       taxPrice: {
-//         needs: { taxPrice: true },
-//         compute(cart) {
-//           return cart.taxPrice.toString();
-//         },
-//       },
-//       totalPrice: {
-//         needs: { totalPrice: true },
-//         compute(cart) {
-//           return cart.totalPrice.toString();
-//         },
-//       },
-//     },
-//     orderItem: {
-//       price: {
-//         compute(cart) {
-//           return cart.price.toString();
-//         },
-//       },
-//     },
-//   },
-// });
-
-export const prisma = new PrismaClient({ adapter });
+export const prisma = new PrismaClient({ adapter }).$extends({
+  result: {
+    product: {
+      price: {
+        compute(product) {
+          return product.price.toString();
+        },
+      },
+      rating: {
+        compute(product) {
+          return product.rating.toString();
+        },
+      },
+    },
+    cart: {
+      itemsPrice: {
+        needs: { itemsPrice: true },
+        compute(cart) {
+          return cart.itemsPrice.toString();
+        },
+      },
+      shippingPrice: {
+        needs: { shippingPrice: true },
+        compute(cart) {
+          return cart.shippingPrice.toString();
+        },
+      },
+      taxPrice: {
+        needs: { taxPrice: true },
+        compute(cart) {
+          return cart.taxPrice.toString();
+        },
+      },
+      totalPrice: {
+        needs: { totalPrice: true },
+        compute(cart) {
+          return cart.totalPrice.toString();
+        },
+      },
+    },
+    order: {
+      itemsPrice: {
+        needs: { itemsPrice: true },
+        compute(cart) {
+          return cart.itemsPrice.toString();
+        },
+      },
+      shippingPrice: {
+        needs: { shippingPrice: true },
+        compute(cart) {
+          return cart.shippingPrice.toString();
+        },
+      },
+      taxPrice: {
+        needs: { taxPrice: true },
+        compute(cart) {
+          return cart.taxPrice.toString();
+        },
+      },
+      totalPrice: {
+        needs: { totalPrice: true },
+        compute(cart) {
+          return cart.totalPrice.toString();
+        },
+      },
+    },
+    orderItem: {
+      price: {
+        compute(cart) {
+          return cart.price.toString();
+        },
+      },
+    },
+  },
+});
